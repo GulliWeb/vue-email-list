031f79ddd94333de6e8d0cefc1f94df29b76d50d
Andrea Gulli <andreagulli11@gmail.com>
2024-09-11 16:56:35 +0200

    complete EX + BONUS

--- script.js
+++ script.js
@@ -2,20 +2,30 @@ const { createApp } = Vue
 
 createApp( {
     data() {
-
         return {
-
+            email: [],
+        
         }
+    },
+
+    methods: {
+        // Prendo la variabile axios importata via CDN, chiamo poi 
+        // il metodo GET per fare una chiamata HTTP GET
+        // Una volta che ho la risposta in maniera asincrona eseguo la funzione anonima di callback all'interno
+        getRandomEmail() {
+            // Stampo 10 volte la mail
+            for (let i = 0; i < 10; i++) {
+                axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then((response) => {
+                    this.email.push(response.data.response)
+                })
+            }
+           
+        }
+    },
+    mounted(){
+        this.getRandomEmail()
     }
+}).mount('#app');
 
-}).mount('#app')
-
-// Prendo la variabile axios importata via CDN, chiamo poi 
-// il metodo GET per fare una chiamata HTTP GET
-// Una volta che ho la risposta in maniera asincrona eseguo la funzione anonima di callback all'interno
-axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-.then(function name(response) {
-    console.log(response)
-
-})
 
